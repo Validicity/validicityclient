3848607a5e5d20e3a0582fecad9e5fd03d857b98
Göran Krampe <goran.krampe@gmail.com>
2020-05-08 18:27:18 +0200

    Better description

--- bin/validicityclient.dart
+++ bin/validicityclient.dart
@@ -11,28 +11,29 @@ printVersion(v) {
 }
 
 main(List<String> arguments) async {
-  var runner = CommandRunner("validicityclient", "Daemon for Validicity.")
-    ..argParser.addOption("config",
-        abbr: "c",
-        defaultsTo: "validicity.yaml",
-        valueHelp: "config file name",
-        callback: (fn) => configFile = fn)
-    ..addCommand(TestNFCCommand())
-    ..addCommand(TestEmulatedKeyboard())
-    ..addCommand(TestContinuous())
-    ..addCommand(CreateKeysCommand())
-    ..addCommand(RegisterCommand())
-    ..addCommand(StatusCommand())
-    ..argParser.addFlag('version',
-        negatable: false,
-        help: 'Show version of validicityclient',
-        callback: printVersion)
-    ..argParser.addFlag('verbose',
-        help: 'Show more information when executing commands',
-        abbr: 'v',
-        defaultsTo: null)
-    ..argParser.addFlag('pretty',
-        help: 'Pretty print JSON in results', abbr: 'p', defaultsTo: null);
+  var runner =
+      CommandRunner("validicityclient", "NFC scanner daemon for Validicity.")
+        ..argParser.addOption("config",
+            abbr: "c",
+            defaultsTo: "validicity.yaml",
+            valueHelp: "config file name",
+            callback: (fn) => configFile = fn)
+        ..addCommand(TestNFCCommand())
+        ..addCommand(TestEmulatedKeyboard())
+        ..addCommand(TestContinuous())
+        ..addCommand(CreateKeysCommand())
+        ..addCommand(RegisterCommand())
+        ..addCommand(StatusCommand())
+        ..argParser.addFlag('version',
+            negatable: false,
+            help: 'Show version of validicityclient',
+            callback: printVersion)
+        ..argParser.addFlag('verbose',
+            help: 'Show more information when executing commands',
+            abbr: 'v',
+            defaultsTo: null)
+        ..argParser.addFlag('pretty',
+            help: 'Pretty print JSON in results', abbr: 'p', defaultsTo: null);
   await runner.run(arguments);
   exit(0);
 }
